329f98596bbc24217c1b88ba4150eb620d78e971
MD ABDUR RAFIQ <35421726+AbdurRafiq@users.noreply.github.com>
2019-09-19 23:06:22 +0600

    Life Module

---
+++ life.py
@@ -0,0 +1,8 @@
+import math  
+
+theAnswer = 5974
+def quote_marvin():
+    print ("Ive calculated your chance of survuval,but I don't think you \' ll like it ")
+    
+def square_root(x):
+    return math.sqrt(x)
